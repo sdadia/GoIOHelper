7fdd597407416933015631fa3d9aab165d537c1f
sahil <sdadia@cameramatics.com>
2022-06-23 12:02:34 +0100

    Made changes

---
+++ JSONHelper.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func printHelloFromModule(data string) {
+	fmt.Printf("Data being sent is : %s\n", data)
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/sdadia/GoIOHelper
+module GoIOHelper
 
 go 1.18
